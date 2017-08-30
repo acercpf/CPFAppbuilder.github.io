8551fb4243eb5691931c091cd98d1f5685c19242
林俊佑 <LinChunYu@linjunyoudeMacBook-Pro.local>
2017-08-30 12:46:28 +0800

    as

--- RobotControl/action.js
+++ RobotControl/action.js
@@ -3,41 +3,47 @@ function setup(){
         var ret = cpf.setPinMode('["resetPin"],["setPinMode", "digital", 4, "OUTPUT"],["setPinMode", "digital", 5, "SERVO"],["setPinMode", "digital", 6, "SERVO"],["setPinMode", "digital", 8, "SERVO"],["setPinMode", "digital", 9, "SERVO"],["setPinMode", "digital", 10, "SERVO"],["setPinMode", "digital", 11, "SERVO"]'); 
 }
 function myFunction1(){
+    document.getElementById("demo").innerHTML="開燈了！";
     cpf.set("socket d4", 1);
     cpf.request ('["digitalwrite", 4, 1]');
-    document.getElementById("demo").innerHTML="開燈了！";
+    
 }
 function myFunction2(){
+    document.getElementById("demo").innerHTML="關燈了！";
     cpf.set("socket d4", 0);
     cpf.request ('["digitalwrite", 4, 0]');
-    document.getElementById("demo").innerHTML="關燈了！";
+    
 }
 function myFunction3(){
-    
+    document.getElementById("demo").innerHTML="放下手";
     cpf.set("socket d10", 0);
     cpf.set("socket d11", 90);
     cpf.request ('["servo_pos", 10, 0],["servo_pos", 11, 90]');
-    document.getElementById("demo").innerHTML="放下手";
+    
 }
 function myFunction4(){
+    document.getElementById("demo").innerHTML="舉起手";
     cpf.set("socket d10", 90);
     cpf.set("socket d11", 0);
     cpf.request ('["servo_pos", 10, 90],["servo_pos", 11, 0]');
-    document.getElementById("demo").innerHTML="舉起手";
+    
 }
 function myFunction5(){
+    document.getElementById("demo").innerHTML="單腳站";
     cpf.set("socket d5", 60);
     cpf.set("socket d6", 120);
     cpf.request ('["servo_pos", 5, 60],["servo_pos", 6, 120]');
-    document.getElementById("demo").innerHTML="單腳站";
+    
 }
 function myFunction6(){
+    document.getElementById("demo").innerHTML="顛腳尖";
     cpf.set("socket d5", 120);
     cpf.set("socket d6", 60);
     cpf.request ('["servo_pos", 5, 120],["servo_pos", 6, 60]');
-    document.getElementById("demo").innerHTML="顛腳尖";
+    
 }
 function myFunction7(){
+    document.getElementById("demo").innerHTML="校正";
     cpf.set("socket d10", 90);
     cpf.set("socket d11", 0);
     cpf.set("socket d5", 120);
@@ -48,5 +54,5 @@ function myFunction7(){
     cpf.set("socket d5", 60);
     cpf.set("socket d6", 120);
     cpf.request ('["servo_pos", 10, 0],["servo_pos", 11, 90],["servo_pos", 5, 60],["servo_pos", 6, 120]');
-    document.getElementById("demo").innerHTML="校正";
+    
 }
